e06f54af59b1b10c91560917dedc4290dcb8ac07
Jonathancg <Jonathancg90@gmail.com>
2013-11-17 22:22:12 -0500

    separate routes

--- app.js
+++ app.js
@@ -5,14 +5,13 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5050);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -28,8 +27,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+require('./routes/index')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
+module.exports =  function(app) {
 
-/*
- * GET home page.
- */
+    app.get('/', function(req, res){
+        res.render('index', { title: 'Lista de comentarios'});
+    });
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
 };
