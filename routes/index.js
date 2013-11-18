cbe2e2b1b8b077f98434a44ae8dcd67fb9674c64
Jonathancg <Jonathancg90@gmail.com>
2013-11-17 22:45:08 -0500

    del chat

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports =  function(app) {
-    var muro = require('../models/chat')(db);
-    
+
     app.get('/', function(req, res){
         res.render('index', { title: 'Lista de comentarios'});
     });
