9bce8722b946939cd3743c35346f79ce96259c58
Jonathancg <Jonathancg90@gmail.com>
2013-11-17 22:43:09 -0500

    add chat

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 module.exports =  function(app) {
-
+    var muro = require('../models/chat')(db);
+    
     app.get('/', function(req, res){
         res.render('index', { title: 'Lista de comentarios'});
     });
