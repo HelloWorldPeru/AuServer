c9ad6fcb0f16c3daf079aeb13430910453f72fc6
Jonathancg <Jonathancg90@gmail.com>
2013-11-18 02:12:49 -0500

    add events socket.io

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ mongo.Db.connect(mongoUri, function (err, db) {
 });
 
 
-//io = io.listen(server);
-//
-//io.configure(function(){
-//	io.disable('log');
-//});
-//
-//require('./controllers/controller')(io);
+io = io.listen(server);
+
+io.configure(function(){
+	io.disable('log');
+});
+
+require('./controllers/controller')(io);
