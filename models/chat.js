a74e3b00b6846320336f542ad42e201c0babfc5c
Jonathancg <Jonathancg90@gmail.com>
2013-11-17 22:31:42 -0500

    add chat

--- models/chat.js
+++ models/chat.js
@@ -1,7 +1,23 @@
-/**
- * Created with PyCharm.
- * User: platano
- * Date: 17/11/13
- * Time: 22:28
- * To change this template use File | Settings | File Templates.
- */
+var muro = {};
+
+module.exports =  function(db){
+    muro.db = db;
+    muro.coments = muro.db.collection('coments');
+    return muro;
+};
+
+muro.new =  function(newData, callBack){
+    muro.coments.insert(newData, function(err, record){
+        callBack(null)
+    });
+};
+
+muro.list =  function(callBack) {
+    muro.coments.find().toArray(function(e, res){
+        if(e) {
+            callBack(e)
+        } else {
+            callBack(null, res);
+        }
+    })
+};
