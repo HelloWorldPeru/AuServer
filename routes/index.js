718eeca0acde1ffdd5039510799557191b0c14d3
Jonathancg <Jonathancg90@gmail.com>
2013-11-17 22:52:32 -0500

    add coments

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,18 @@ module.exports =  function(app, db) {
     var muro = require('../models/chat')(db);
 
     app.get('/', function(req, res){
-        res.render('index', { title: 'Lista de comentarios'});
+        muro.list(function(e, coments){
+            res.render('index', { title: 'Lista de comentarios', comentarios: coments});
+        });
+    });
+
+
+    app.post('/', function(req, res){
+        muro.new({name: req.param('name'), comentario:req.param('comentario')}, function(e){
+            muro.list(function(e, coments){
+                res.render('index', { title: 'Lista de comentarios', comentarios: coments });
+            });
+        });
     });
 
 };
